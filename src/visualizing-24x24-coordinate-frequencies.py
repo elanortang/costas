454d51aaa38757444a288f9cfc72ee5794ef5c37
Rose Silver <silver.r@northeastern.edu>
2024-12-04 08:22:52 -0500

    partial progress before heading into the office

--- src/visualizing-24x24-coordinate-frequencies.py
+++ src/visualizing-24x24-coordinate-frequencies.py
@@ -3,7 +3,7 @@ import numpy as np
 import csv
 
 # Specify the path to your CSV file
-file_path = "coord_freq_24.csv"
+file_path = "/Users/rosesilver/Desktop/costas/results/coord_freq_24.csv"
 
 # Initialize a set to store the data
 data_set = set()
@@ -22,14 +22,15 @@ with open(file_path, mode="r") as file:
 
 # Unpack the data
 x, y, values = zip(*data_set)
-colors = ['white' if value > 5 else 'blue' for value in values]
+#colors = ['white' if value > 5 else 'blue' for value in values]
+
 # Create a scatter plot with a colormap
 plt.figure(figsize=(6, 6))
-sc = plt.scatter(x, y, c=colors, cmap="viridis", s=200, edgecolor="k")
-#plt.colorbar(sc, label="Value")
+sc = plt.scatter(x, y, c=values, cmap="viridis", s=200, edgecolor="k")
+plt.colorbar(sc, label="Value")
 plt.xlabel("X")
 plt.ylabel("Y")
-plt.title("Coordinates with frequencies at most 5")
+plt.title("Coordinate frequencies for n=24")
 plt.grid(True)
-plt.savefig("coord_freq_atmost5.png")
+plt.savefig("coord_freqs_24.png")
 #plt.show()
